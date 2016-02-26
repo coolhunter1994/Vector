6027a26ed1fba7f65b46b10810d5ee686aac87b9
coolhunter1994 <coolhunter1994@yandex.ru>
2016-02-26 12:12:08 +0300

    Update test_vector.cpp

--- tests/test_vector.cpp
+++ tests/test_vector.cpp
@@ -1,7 +1,7 @@
 #define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
 
 #include "catch.hpp"
-#include "vector.h"
+#include "../include/vector.h"
 
 typedef double Vec3[3];
 
